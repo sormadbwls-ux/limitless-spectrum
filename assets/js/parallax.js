28455f800a9acd4341b5bb5009d95064e18ba725
진 <jin@jin-ui-iMac.local>
2026-06-05 21:21:42 +0900

    Inital portfolio setup

---
+++ assets/js/parallax.js
@@ -0,0 +1,13 @@
+console.log("parallax loaded");
+
+const hero = document.querySelector(".hero");
+
+document.addEventListener("mousemove", (e) => {
+
+    const x = (window.innerWidth / 2 - e.clientX) / 120;
+    const y = (window.innerHeight / 2 - e.clientY) / 120;
+
+    hero.style.backgroundPosition =
+        `calc(50% + ${x}px) calc(50% + ${y}px)`;
+
+});
