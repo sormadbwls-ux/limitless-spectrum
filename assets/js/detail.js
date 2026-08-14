1d8a7c5fcdf86959dd1698b1d12e1c996dcc8d46
진 <jin@jin-ui-iMac.local>
2026-08-14 21:22:48 +0900

    finalize project detail page

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -11,27 +11,39 @@ if (topBtn) {
 
 
 // 이미지 클릭시 미리보기 기능
-const workImage = document.querySelector(".detail-image");
+const workImages = document.querySelectorAll(".detail-image");
 const imageModal = document.querySelector("#imageModal");
 const modalImage = document.querySelector("#modalImage");
 const imageModalClose = document.querySelector("#imageModalClose");
 
-if (workImage && imageModal && modalImage && imageModalClose) {
+if (workImages.length && imageModal && modalImage && imageModalClose) {
 
-    workImage.addEventListener("click", () => {
-        modalImage.src = workImage.src;
-        imageModal.classList.add("active");
-        document.body.style.overflow = "hidden";
+    workImages.forEach((workImage) => {
+
+        workImage.addEventListener("click", () => {
+            modalImage.src = workImage.src;
+
+            imageModal.classList.add("active");
+
+            document.body.style.overflow = "hidden";
+        });
+
     });
 
     imageModalClose.addEventListener("click", () => {
         imageModal.classList.remove("active");
+
         document.body.style.overflow = "";
     });
 
- imageModal.addEventListener("click", () => {
-    imageModal.classList.remove("active");
-    document.body.style.overflow = "";
-});
+    imageModal.addEventListener("click", (e) => {
+
+        if (e.target === imageModal) {
+            imageModal.classList.remove("active");
+
+            document.body.style.overflow = "";
+        }
+
+    });
 
 }
