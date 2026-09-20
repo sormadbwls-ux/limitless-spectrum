7f89cd6e251d9b5c4445b8629527730f8a2dd121
진 <jin@jin-ui-iMac.local>
2026-09-21 04:20:31 +0900

    mobile_menu_

--- assets/js/menu.js
+++
@@ -1,8 +0,0 @@
-const menuButton = document.querySelector(".menu-toggle");
-const header = document.querySelector("header");
-
-if (menuButton && header) {
-    menuButton.addEventListener("click", () => {
-        header.classList.toggle("menu-open");
-    });
-}
